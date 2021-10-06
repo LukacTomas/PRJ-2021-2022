ed34134523d47984d32a7258dd574342b2492ede
LukacTomas <lukactv@gmail.com>
2021-10-06 08:49:12 +0200

    Hodina 1 - Hello.c

---
+++ Hodina 1/hello.c
@@ -0,0 +1,9 @@
+#include <stdio.h> // kniznica STanDard Input a Output
+
+
+// hlavna funkcia, tu sa vykonava nas program
+int main(void)
+{
+    printf("Ahojte, Tretiaci\n");
+    printf("Ako \t sa \tgcc mate\n");
+}
