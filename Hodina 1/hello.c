fc40337426400b3775a60e5dfe63dbe2be8a834a
LukacTomas <lukactv@gmail.com>
2021-10-06 08:58:03 +0200

    comments added

--- Hodina 1/hello.c
+++ Hodina 1/hello.c
@@ -4,6 +4,9 @@
 // hlavna funkcia, tu sa vykonava nas program
 int main(void)
 {
+    // printf sluzi na vypis do stdout
     printf("Ahojte, Tretiaci\n");
+
+    // \t je tabulator \n je new line
     printf("Ako \t sa \tgcc mate\n");
 }
